6f675bde64e7a41e0271e1019bcb62f6aaa60a8e
구본엽 <piusku2@gmail.com>
2025-06-26 18:06:43 +0900

    ㅇ

--- Movietalk/src/main/java/com/sec/movietalk/common/config/SecurityConfig.java
+++ Movietalk/src/main/java/com/sec/movietalk/common/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.sec.movietalk.userinfo.config;
+package com.sec.movietalk.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
