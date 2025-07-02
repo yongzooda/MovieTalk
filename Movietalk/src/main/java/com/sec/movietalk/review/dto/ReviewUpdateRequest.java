353db7ccfac473078d93515e57886bb5ed4d404f
sihwan0816 <sihwan4472@daum.net>
2025-07-02 17:28:41 +0900

    삭제, 수정버튼 추가

--- Movietalk/src/main/java/com/sec/movietalk/review/dto/ReviewUpdateRequest.java
+++ Movietalk/src/main/java/com/sec/movietalk/review/dto/ReviewUpdateRequest.java
@@ -1,17 +1,20 @@
 package com.sec.movietalk.review.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class ReviewUpdateRequest {
 
     private Long reviewId;
+    private Long userId;
+    private String movieTitle;
     private String content;
-
-
 }
+
 
 
 
