faf347d1509169bec7a283983c81e7ec5c6740c3
guwls <guswls2654@naver.com>
2025-07-03 14:05:03 +0900

    배우 cache 테이블 구현

---
+++ Movietalk/src/main/java/com/sec/movietalk/actor/dto/ActorCache.java
@@ -0,0 +1,58 @@
+package com.sec.movietalk.actor.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "actor_cache")
+public class ActorCache {
+    @Id
+    private Long id;
+
+    private String name;
+
+    private Integer gender;
+
+    @Column(name = "profile_path")
+    private String profilePath;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public String getProfilePath() {
+        return profilePath;
+    }
+
+    public void setProfilePath(String profilePath) {
+        this.profilePath = profilePath;
+    }
+}
