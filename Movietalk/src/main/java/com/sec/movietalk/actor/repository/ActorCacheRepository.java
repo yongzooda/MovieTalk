203a9b208e8c03404dcd8c155d1cf96e540f83e5
구본엽 <piusku2@gmail.com>
2025-07-03 18:24:47 +0900

    메인 화면에 배우 출력

--- Movietalk/src/main/java/com/sec/movietalk/actor/repository/ActorCacheRepository.java
+++ Movietalk/src/main/java/com/sec/movietalk/actor/repository/ActorCacheRepository.java
@@ -1,8 +1,12 @@
 package com.sec.movietalk.actor.repository;
 
-import com.sec.movietalk.actor.dto.ActorCache;
+import com.sec.movietalk.common.domain.actor.ActorCache;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ActorCacheRepository extends JpaRepository<ActorCache, Long> {
     boolean existsById(Long id);
+
+    List<ActorCache> findByIdIn(List<Integer> actorIds);
 }
